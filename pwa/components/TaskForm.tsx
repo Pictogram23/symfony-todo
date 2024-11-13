ffb3dd0bc89bb752235927a1c77a4f44ef2373a1
Pictogram23 <shanchengyinger@gmail.com>
2024-11-13 09:49:28 +0900

    入力フォームをモーダルに移動

--- pwa/components/TaskForm.tsx
+++ pwa/components/TaskForm.tsx
@@ -3,9 +3,10 @@ import { useState } from "react"
 
 type TaskFormProps = {
     getTasks: () => void,
+    handleCloseModal: () => void,
 }
 
-const TaskForm = ({getTasks}: TaskFormProps) => {
+const TaskForm = ({getTasks, handleCloseModal}: TaskFormProps) => {
     const [name, setName] = useState<string>('')
     const [desc, setDesc] = useState<string>('')
     const [deadline, setDeadline] = useState<string>()
@@ -28,16 +29,22 @@ const TaskForm = ({getTasks}: TaskFormProps) => {
         }
         await fetch('/tasks', options)
         await getTasks()
+
+        handleCloseModal()
     }
 
     return (
-        <Box component='form' noValidate autoComplete='off'
-            sx={{width: 275, display: 'flex', alignItems: 'center', flexFlow: 'column', margin: 'auto'}}>
-            <TextField label="Name" onChange={(e) => {setName(e.target.value)}} sx={{margin: 'auto', width: 275}} />
-            <TextareaAutosize onChange={(e) => {setDesc(e.target.value)}} style={{margin: 'auto', width: 275}} />
-            <TextField type="date" onChange={(e) => setDeadline(e.target.value)} style={{margin: 'auto', width: 275}} />
-            <Button variant='outlined' onClick={POST} sx={{width: 275}}>Submit</Button>
-        </Box>
+        <div style={{position: 'relative', width: 400, height: 300}}>
+            <Box component='form' noValidate autoComplete='off'
+                sx={{width: 275, display: 'flex', alignItems: 'center', flexFlow: 'column', margin: 'auto',
+                    position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)'
+                }}>
+                <TextField label="Name" onChange={(e) => {setName(e.target.value)}} sx={{margin: 'auto', width: 275}} />
+                <TextareaAutosize onChange={(e) => {setDesc(e.target.value)}} style={{margin: 'auto', width: 275}} />
+                <TextField type="date" onChange={(e) => setDeadline(e.target.value)} style={{margin: 'auto', width: 275}} />
+                <Button variant='outlined' onClick={POST} sx={{width: 275}}>Submit</Button>
+            </Box>
+        </div>
     )
 }
 
--- pwa/pages/index.tsx
+++ pwa/pages/index.tsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from "react"
 import TaskCard from "../components/TaskCard"
-import { Card } from "@mui/material"
+import { Box, Card, IconButton, Modal } from "@mui/material"
 import TaskForm from "../components/TaskForm"
+import AddCircleIcon from '@mui/icons-material/AddCircle';
 
 type Task = {
   id: number,
@@ -13,6 +14,10 @@ type Task = {
 
 const Home = () => {
   const [tasks, setTasks] = useState<Task[]>([])
+  const [modalOpen, setModalOpen] = useState<boolean>(false)
+
+  const handleOpenModal = () => setModalOpen(true)
+  const handleCloseModal = () => setModalOpen(false)
 
   const getTasks = async() => {
     const res = await fetch("/tasks")
@@ -44,9 +49,16 @@ const Home = () => {
         )
       })}
     </div>
-    <div style={{paddingTop: 10}}>
-      <TaskForm getTasks={getTasks} />
-    </div>
+    <IconButton onClick={handleOpenModal}>
+      <AddCircleIcon />
+    </IconButton>
+    <Modal open={modalOpen} onClose={handleCloseModal}>
+      <Box position='absolute' top='50%' left='50%'
+        width={400} height={300} bgcolor='background.paper'
+        sx={{transform: 'translate(-50%, -50%)'}}>
+        <TaskForm getTasks={getTasks} handleCloseModal={handleCloseModal} />
+      </Box>
+    </Modal>
   </>)
 }
 
